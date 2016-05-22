0ade911994bc87e4610bb55d1e49cc7c755847b2
Mason McGlothlin <mason.mcglothlin@gmail.com>
2016-05-21 22:33:13 -0500

    Added Java project to VS solution.

---
+++ src/SpringSignalRServer/ClassLibrary1/src/main/java/pkg/JavaBridge.java
@@ -0,0 +1,213 @@
+package javaai;
+
+import com.springrts.ai.oo.*;
+import com.springrts.ai.oo.clb.*;
+import java.util.List;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import microsoft.aspnet.signalr.client.*;
+import microsoft.aspnet.signalr.client.hubs.*;
+import microsoft.aspnet.signalr.client.transport.*;
+
+public class JavaBridge extends AbstractOOAI {
+    
+    private OOAICallback Callback;
+    private static final String LogLocation = "C:\\Users\\msm8b\\Documents\\JavaBridgeLog.txt";
+    private HubProxy MyHubProxy;
+    
+    public static void InitializeLogFile()
+    {
+        try
+        {
+            Writer output = new BufferedWriter(new FileWriter(LogLocation));
+            output.close();
+        }
+        catch(IOException ex){
+            //cant do anything here
+        }
+    }
+    
+    public static void Log(String message) {
+        try
+        {
+            Writer output = new BufferedWriter(new FileWriter(LogLocation, true));
+            output.append(message + "\n");
+            output.close();
+        }
+        catch(IOException ex){
+            //cant do anything here
+        }
+    }
+    
+    private void InitializeSignalR()
+    {
+        Log("Initializing SignalR");
+        HubConnection connection = new HubConnection("http://localhost:8080");
+        MyHubProxy = connection.createHubProxy("GameHub");
+        ClientTransport transport = new ServerSentEventsTransport(connection.getLogger());
+        SignalRFuture<Void> awaitConnection = connection.start(transport);
+        // when do we close the connection?!
+        try
+        {
+            awaitConnection.get();
+        }
+        catch (InterruptedException e)
+        {
+            Log(e.getMessage());           
+        }
+        catch (ExecutionException e)
+        {
+             Log(e.getMessage());
+        }
+        Log("SignalR initialized");
+    }
+        
+    @Override
+    public int init(int teamId, OOAICallback callback) {
+        InitializeLogFile();
+        Log("Initializing");
+        InitializeSignalR();
+        Callback = callback;
+        Log("Initialized");
+        return 0;
+    }
+
+    @Override
+    public int unitFinished(Unit unit) {
+        return 0;
+    }
+
+    @Override
+    public int commandFinished(Unit arg0, int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public int enemyCreated(Unit arg0) {
+        return 0;
+    }
+
+    @Override
+    public int enemyDamaged(Unit unit, Unit unit1, float f, AIFloat3 aif, WeaponDef wd, boolean bln) {
+        return 0;
+    }
+
+    @Override
+    public int enemyDestroyed(Unit unit, Unit uni1) {
+
+        return 0;
+    }
+
+    @Override
+    public int enemyEnterLOS(Unit unit) {
+        return 0;
+    }
+
+    @Override
+    public int enemyEnterRadar(Unit unit) {
+        return 0;
+    }
+
+    @Override
+    public int enemyFinished(Unit unit) {
+        return 0;
+    }
+
+    @Override
+    public int enemyLeaveLOS(Unit unit) {
+        return 0;
+    }
+
+    @Override
+    public int enemyLeaveRadar(Unit unit) {
+        return 0;
+    }
+
+    @Override
+    public int save(String string) {
+        return 0;
+    }
+
+    @Override
+    public int seismicPing(AIFloat3 aif, float f) {
+        return 0;
+    }
+
+    @Override
+    public int playerCommand(List<Unit> list, int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public int weaponFired(Unit unit, WeaponDef wd) {
+        return 0;
+    }
+
+    @Override
+    public int unitCaptured(Unit unit, int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public int unitGiven(Unit unit, int i, int i1) {
+        return 0;
+    }
+
+    @Override
+    public int unitDestroyed(Unit unit, Unit unit1) {
+        return 0;
+    }
+
+    @Override
+    public int unitDamaged(Unit unit, Unit unit1, float f, AIFloat3 aif, WeaponDef wd, boolean bln) {
+        return 0;
+    }
+
+    @Override
+    public int unitMoveFailed(Unit unit) {
+        return 0;
+    }
+
+    @Override
+    public int unitIdle(Unit unit) {
+        return 0;
+    }
+
+    @Override
+    public int unitCreated(Unit unit, Unit unit1) {
+        return 0;
+    }
+
+    @Override
+    public int message(int i, String string) {
+        return 0;
+    }
+
+    @Override
+    public int update(int i) {
+        if(i == 0 )
+        {
+            Callback.getGame().sendTextMessage("Hello, world!", 0);
+        }
+        MyHubProxy.invoke("Update", i);
+        return 0;
+    }
+
+    @Override
+    public int release(int i) {
+        return 0;
+    }
+
+    @Override
+    public int load(String string) {
+        return 0;
+    }
+
+    @Override
+    public int luaMessage(String string) {
+        return 0;
+    }
+}
